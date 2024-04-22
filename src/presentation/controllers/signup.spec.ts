25eb39e0fe54abb701cddd5dccd95483ca089766
jeanmbiz <jeanmbiz@hotmail.com>
2024-04-22 08:36:41 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provider', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'jeanmbiz@hotmail.com',
+        password: 'jmb1987',
+        passwordConfirmation: 'jmb1987'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
