8a9bbd913c6f18e5e421b566391fba7f7e1fc0fd
jeanmbiz <jeanmbiz@hotmail.com>
2024-04-22 08:49:07 -0300

    test: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missin param: name'))
   })
+
+  test('Should return 400 if no email is provider', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'jean',
+        password: 'jmb1987',
+        passwordConfirmation: 'jmb1987'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missin param: email'))
+  })
 })
