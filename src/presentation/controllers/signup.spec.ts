2352cf721faf521ca4354ce29d173951cbf93ae7
jeanmbiz <jeanmbiz@hotmail.com>
2024-04-22 08:49:51 -0300

    teste

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,31 +1,31 @@
-import { SignUpController } from './signup'
+import { SignUpController } from "./signup";
 
-describe('SignUp Controller', () => {
-  test('Should return 400 if no name is provider', () => {
-    const sut = new SignUpController()
+describe("SignUp Controller", () => {
+  test("Should return 400 if no name is provider", () => {
+    const sut = new SignUpController();
     const httpRequest = {
       body: {
-        email: 'jeanmbiz@hotmail.com',
-        password: 'jmb1987',
-        passwordConfirmation: 'jmb1987'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missin param: name'))
-  })
+        email: "jeanmbiz@hotmail.com",
+        password: "jmb1987",
+        passwordConfirmation: "jmb1987",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missin param: name"));
+  });
 
-  test('Should return 400 if no email is provider', () => {
-    const sut = new SignUpController()
+  test("Should return 400 if no email is provider", () => {
+    const sut = new SignUpController();
     const httpRequest = {
       body: {
-        name: 'jean',
-        password: 'jmb1987',
-        passwordConfirmation: 'jmb1987'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missin param: email'))
-  })
-})
+        name: "jean",
+        password: "jmb1987",
+        passwordConfirmation: "jmb1987",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missin param: email"));
+  });
+});
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,16 @@
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle(httpRequest: any): any {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missin param: name')
-      }
+        body: new Error("Missin param: name"),
+      };
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missin param: email')
-      }
+        body: new Error("Missin param: email"),
+      };
     }
   }
 }
