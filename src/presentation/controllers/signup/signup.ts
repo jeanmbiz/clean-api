8262539ffb12a12941602ba8b7a226b60c946ac6
jeanmbiz <jeanmbiz@hotmail.com>
2024-04-24 08:37:50 -0300

    feat: ensure SignupController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -42,7 +42,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
@@ -50,7 +50,7 @@ export class SignUpController implements Controller {
 
       return {
         statusCode: 200,
-        body: 'Success'
+        body: account
       }
     } catch (error) {
       return serverError()
