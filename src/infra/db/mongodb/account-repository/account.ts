3c3eaca9e7a72a1e21a735b81d25551c7a1c7e7c
jeanmbiz <jeanmbiz@hotmail.com>
2024-04-30 06:56:08 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,19 @@
+import { AccountModel } from './../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AddAccountModel } from './../../../../domain/usecases/add-account'
+import { AddAccountRepository } from '@/data/protocols/add-account-repository'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = await accountCollection.findOne({ _id: result.insertedId })
+
+    if (!account) {
+      throw new Error('Account not found after insertion.')
+    }
+
+    const { _id, name, email, password } = account
+    return { id: _id.toString(), name, email, password }
+  }
+}
