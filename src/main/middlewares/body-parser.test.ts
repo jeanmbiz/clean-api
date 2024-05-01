6d87d1b977a3c428bbb4c69440c512b2852bd8b7
jeanmbiz <jeanmbiz@hotmail.com>
2024-05-01 09:59:58 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -2,13 +2,22 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
-  test('Should parse body as json ', async () => {
-    app.post('/test_body_parser', (req, res) => {
-      res.send(req.body)
+  test('Should parse body as json', async () => {
+    app.post('/test_content_type', (req, res) => {
+      res.send('')
     })
     await request(app)
-      .post('/test_body_parser')
-      .send({ name: 'Jean' })
-      .expect({ name: 'Jean' })
+      .post('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type when forced', async () => {
+    app.post('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .post('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
