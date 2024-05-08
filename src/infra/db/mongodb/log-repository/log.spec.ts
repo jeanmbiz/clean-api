df5d1caa0a232053a2adb06dfaa3b57be4e4e373
jeanmbiz <jeanmbiz@hotmail.com>
2024-05-08 08:36:01 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,32 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MongoDB URL is not defined in environment variables.')
+    }
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
