cc6cd3fdd519a8d218b7d104660c581a2007fbef
jeanmbiz <jeanmbiz@hotmail.com>
2024-05-27 06:54:29 -0300

    test:ensure RequiredFieldValidation not return id validation succeds

--- src/presentation/helpers/validators/required-fiield-validation.spec.ts
+++ src/presentation/helpers/validators/required-fiield-validation.spec.ts
@@ -2,9 +2,15 @@ import { MissingParamError } from '../../../presentation/errors'
 import { RequiredFieldValidation } from './required-fiield-validation'
 
 describe('RequiredField Validation', () => {
-  test('Should return a MissingParamError if validation fails ', () => {
+  test('Should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('field')
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validation success', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
   })
 })
