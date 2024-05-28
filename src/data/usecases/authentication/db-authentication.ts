2e713b0db1ec65ae121d618219fa17fde842b0c3
jeanmbiz <jeanmbiz@hotmail.com>
2024-05-28 09:59:29 -0300

    feat: ensure DbAuhentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -5,16 +5,22 @@ import {
 } from '../../../domain/usecases/authentication'
 import { HashComparer } from '../../../data/protocols/criptography/hash-comparer'
 import { TokenGenerator } from '../../../data/protocols/criptography/token-generator'
+import { UpdateAccessTokenRepository } from '../../../data/protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenRepositoryStub: UpdateAccessTokenRepository
 
-  constructor (loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  constructor (loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator,
+    updateAccessTokenRepositoryStub: UpdateAccessTokenRepository) {
     this.loadAccountByEmailRepositoryStub = loadAccountByEmailRepositoryStub
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepositoryStub = updateAccessTokenRepositoryStub
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -23,6 +29,7 @@ export class DbAuthentication implements Authentication {
       const valid = await this.hashComparer.compare(authentication.password, account.password)
       if (valid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepositoryStub.update(account.id, accessToken)
         return accessToken
       }
     }
